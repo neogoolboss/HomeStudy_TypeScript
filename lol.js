1d54729eb9f979e01f3b92ce4d8c031fdbdde5cc
neogoolboss <krossjsnet88@gmail.com>
2024-08-09 20:34:09 +0900

    script

---
+++ lol.js
@@ -0,0 +1,15 @@
+function 기계(q, w) {
+    this.q = q;
+    this.w = w;
+}
+var nunu = new 기계('consume', 'snowball');
+var garen = new 기계('strike', 'courage');
+// var nunu = {
+//     q : 'consume',
+//     w : 'snowball',
+// }
+// var garen = {
+//     q : 'strike',
+//     w : 'courage',
+// }
+// 비슷한 object 많이 만들 일 있으면 class 만들어서 쓰자.
---
+++ lol.ts
@@ -0,0 +1,19 @@
+
+function 기계() {
+    this.q = 'consume';
+    this.w = 'snowball';
+}
+
+var nunu = new 기계();
+
+// var nunu = {
+//     q : 'consume',
+//     w : 'snowball',
+// }
+
+// var garen = {
+//     q : 'strike',
+//     w : 'courage',
+// }
+
+// 비슷한 object 많이 만들 일 있으면 class 만들어서 쓰자.
